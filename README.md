be3dec870e0a1914477e3fa79dad811193a974d6
Marcin Bojko <marcin.bojko@eleader.biz>
2016-06-09 14:31:19 +0200

    Version 1.09

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 <h1> Batch script for default settings in new Hyper-V 2012 R2 instalations
+
+## Maintainer
+* Marcin Bojko
+
 ## Changelog
-* 2016-05-25
+
+* 2016-06-06 version 1.09
+	* Syntax checking for Group Rules Firewall Install
+	* switch to Powershell for the rest of the script
+
+* 2016-05-25 version 1.08
 	* Switching to PowerShell for firewall features.
 	* Install chocolatey at the end
 	
--- default_set.bat
+++ default_set.bat
@@ -1,32 +1,35 @@
-﻿::(C) Marcin Bojko
-::$VER 1.08
-::2016-06-25
-
+﻿:: (C) Marcin Bojko
+:: $VER 1.09
+:: 2016-06-06
+@echo off
 cd /d %~dp0
 
-@echo off
+
 
 :: Hyper-V default firewall settings
-netsh advfirewall set currentprofile settings remotemanagement enable
-@powershell Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-@powershell Enable-NetFirewallRule -DisplayGroup "File and Printer Sharing" 
-@powershell Enable-NetFirewallRule -DisplayGroup "Remote Service Management" 
-@powershell Enable-NetFirewallRule -DisplayGroup "Performance Logs and Alerts" 
-@powershell Enable-NetFirewallRule -DisplayGroup "Remote Event Log Management" 
-@powershell Enable-NetFirewallRule -DisplayGroup "Remote Scheduled Tasks Management"
-@powershell Enable-NetFirewallRule -DisplayGroup "Remote Volume Management"
-@powershell Enable-NetFirewallRule -DisplayGroup "Windows Firewall Remote Management"
-@powershell Enable-NetFirewallRule -DisplayGroup "Windows Management Instrumentation (WMI)"
-@powershell Enable-NetFirewallRule -DisplayGroup "Remote Administration"
-@powershell Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-@powershell Enable-NetFirewallRule -DisplayGroup "Hyper-v Replica HTTP"
-@powershell Enable-NetFirewallRule -DisplayGroup "Hyper-v Replica HTTPS"
+:: netsh advfirewall set currentprofile settings remotemanagement enable
+@powershell Enable-NetFirewallRule -DisplayGroup "'Remote Desktop'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'File and Printer Sharing'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'Remote Service Management'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'Performance Logs and Alerts'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'Remote Event Log Management'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'Remote Scheduled Tasks Management'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'Remote Volume Management'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'Windows Firewall Remote Management'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'Windows Management Instrumentation (WMI)'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'Remote Service Management'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'Hyper-v Replica HTTP'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'Hyper-v Replica HTTPS'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'iSCSI Service'" -Direction "'Outbound'"
+@powershell Enable-NetFirewallRule -DisplayGroup "'iSCSI Service'" -Direction "'Inbound'"
+
+
 
 :: ISCSI settings
-netsh advfirewall firewall set rule group="iSCSI Service" new enable=yes
+:: netsh advfirewall firewall set rule group="iSCSI Service" new enable=yes
 
 ::SNMP
-dism /online /enable-feature /featurename:SNMP
+:: dism /online /enable-feature /featurename:SNMP
 
 :: Allow PING
 netsh firewall set icmpsetting 8
@@ -37,34 +40,34 @@ netsh firewall set icmpsetting 8
 
 
 :: Install features
-powershell Install-WindowsFeature net-framework-core
-powershell Install-WindowsFeature net-framework-features
-powershell Install-WindowsFeature EnhancedStorage -IncludeManagementTools
-powershell Install-WindowsFeature Failover-Clustering -IncludeManagementTools
-powershell Install-WindowsFeature Multipath-IO -IncludeManagementTools
-powershell Install-WindowsFeature SNMP-Service -IncludeManagementTools
-powershell Install-WindowsFeature SNMP-WMI-Provider -IncludeManagementTools
-powershell Install-WindowsFeature Telnet-Client 
-powershell Install-WindowsFeature Windows-Server-Backup
-powershell Install-WindowsFeature RSAT-Role-Tools
-powershell Install-WindowsFeature RSAT-Hyper-V-Tools
-powershell Install-WindowsFeature Hyper-V-PowerShell
-powershell Install-WindowsFeature PowerShell-V2 
+@powershell Install-WindowsFeature net-framework-core
+@powershell Install-WindowsFeature net-framework-features
+@powershell Install-WindowsFeature EnhancedStorage -IncludeManagementTools
+@powershell Install-WindowsFeature Failover-Clustering -IncludeManagementTools
+@powershell Install-WindowsFeature Multipath-IO -IncludeManagementTools
+@powershell Install-WindowsFeature SNMP-Service -IncludeManagementTools
+@powershell Install-WindowsFeature SNMP-WMI-Provider -IncludeManagementTools
+@powershell Install-WindowsFeature Telnet-Client
+:: powershell Install-WindowsFeature Windows-Server-Backup
+@powershell Install-WindowsFeature RSAT-Role-Tools
+:: powershell Install-WindowsFeature RSAT-Hyper-V-Tools
+:: powershell Install-WindowsFeature Hyper-V-PowerShell
+@powershell Install-WindowsFeature PowerShell-V2
 
 
 :: Start Services
-powershell Set-Service -Name MSiSCSI -StartupType Automatic
-powershell Start-Service MSiSCSI
-powershell Set-Service -Name vds -StartupType Automatic
-powershell Start-Service vds
+@powershell Set-Service -Name MSiSCSI -StartupType Automatic
+@powershell Start-Service MSiSCSI
+@powershell Set-Service -Name vds -StartupType Automatic
+@powershell Start-Service vds
 
-:: Set authentication
-winrm set winrm/config/service/auth @{CredSSP="true"}
- 
+
 :: Install chocolatey
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 :: Install puppet and configure to access foreman.eleader.lan
-choco install puppet -ia '"PUPPET_MASTER_SERVER=foreman.office.eleader.biz"' -y
+choco install puppet -ia '"PUPPET_MASTER_SERVER=foreman.eleader.lan"' -y
 choco install doublecmd
 
+:: Set authentication
+winrm set winrm/config/service/auth @{CredSSP="true"}
